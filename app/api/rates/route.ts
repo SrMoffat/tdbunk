e6f10a160283f9035f50b6c99ae37a5ab576df84
SrMoffat <ngigemoffat@gmail.com>
2024-08-19 22:29:44 +0300

    feat(conversions): add 3rd party conversion call

--- app/api/rates/route.ts
+++ app/api/rates/route.ts
@@ -5,9 +5,11 @@ import { NextResponse } from "next/server";
 
 const EXCHANGE_RATE_API_KEY = process.env.EXCHANGE_RATE_API_KEY || '58faa419858f91e90f7ab302'
 const EXCHANGE_RATE_API_URL = 'https://v6.exchangerate-api.com/v6'
+const CRYPTO_EXCHANGE_RATE_API_URL = 'https://api.coingate.com/api/v2/rates/merchant'
 
-interface RequestContext {}
+interface RequestContext { }
 
+const handleGet = createEdgeRouter<NextRequest, RequestContext>();
 const handlePost = createEdgeRouter<NextRequest, RequestContext>();
 
 handlePost
@@ -32,6 +34,41 @@ export async function POST(request: NextRequest, ctx: RequestContext) {
     } catch (error: any) {
         // TODO: Better error handling
         console.log("POST failed", error)
+        return Response.json({ error })
+    }
+}
+
+handleGet
+    .use(async (req, event, next) => {
+        cors()
+        return await next();
+    })
+    .get(async (req) => {
+        const { searchParams } = new URL(req.url);
+
+        const source = searchParams.get("source")
+        const destination = searchParams.get("destination")
+
+        const url = `${CRYPTO_EXCHANGE_RATE_API_URL}/${source}/${destination}`
+
+        const response = await fetch(url);
+        const rate = await response.text();
+
+        const result = {
+            source,
+            destination,
+            rate: parseFloat(rate)
+        }
+
+        return NextResponse.json(result)
+    })
+
+export async function GET(request: NextRequest, ctx: RequestContext) {
+    try {
+        return handleGet.run(request, ctx);
+    } catch (error: any) {
+        // TODO: Better error handling
+        console.log("GET failed", error)
         return Response.json({ error })
     }
 }
